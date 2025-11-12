7044de0f6c0670a61258f62ec9e631d3603c4b4a
moulikesavarapu363 <moulikesavarapu@gmail.com>
2025-11-12 20:03:36 +0530

    Add files via upload

---
+++ bubble sorting.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+    int a[10], i, j, n,temp;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+for(i=0;i<n;i++)
+{
+	for(j=0;j<n-1;j++)
+	{
+		if(a[j]>a[j+1]){
+			temp=a[j];
+			a[j]=a[j+1];
+			a[j+1]=temp;
+		}
+	}
+}
+printf("The sorting order is:\n");
+for(i=0;i<n;i++)
+printf("%d\t",a[i]);
+return 0;
+}
---
+++ finding out duplicate numbers in array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int a[10],n,i,j;
+	scanf("%d",&n);
+	for (i=0;i<n;i++)
+		scanf("%d",&a[i]);
+		printf("Duplicate element in the array are:\n");
+		for(i=0;i<n;i++){
+			for(j=i+1;j<n;j++){
+				if(a[i]==a[j]){
+					printf("%d\n",a[i]);
+					break;
+				}
+			}
+		}
+	return 0;
+}
